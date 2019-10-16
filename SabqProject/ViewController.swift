0e8de7027162a6c2f4e0b0ea1864717145c096b2
sara <sara.galal@ibtikar.net.sa>
2019-10-16 15:38:17 +0200

    Initial Commit

---
+++ SabqProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SabqProject
+//
+//  Created by sara.galal on 10/16/19.
+//  Copyright © 2019 sara.galal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SabqProjectTests/SabqProjectTests.swift
@@ -0,0 +1,34 @@
+//
+//  SabqProjectTests.swift
+//  SabqProjectTests
+//
+//  Created by sara.galal on 10/16/19.
+//  Copyright © 2019 sara.galal. All rights reserved.
+//
+
+import XCTest
+@testable import SabqProject
+
+class SabqProjectTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
